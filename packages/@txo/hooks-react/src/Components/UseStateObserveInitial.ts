ba41ced9696662cce28caca6638d69818196cebe
Erik Slovak <erik.slovak@technologystudio.sk>
2021-09-01 18:52:01 +0200

    add useStateObserveInitial hook

---
+++ packages/@txo/hooks-react/src/Components/UseStateObserveInitial.ts
@@ -0,0 +1,27 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2021-09-01T18:09:96+02:00
+ * @Copyright: Technology Studio
+**/
+
+import {
+  Dispatch,
+  useState,
+  useCallback,
+  useLayoutEffect,
+} from 'react'
+
+import { useLatest } from './UseLatest'
+
+export const useStateObserveInitial = <S,>(initialState: S): [S, Dispatch<S>] => {
+  const recentState = useLatest<S>(initialState)
+  const [state, _setState] = useState<S>(initialState)
+  const setState: Dispatch<S> = useCallback((value: S) => {
+    _setState(value)
+    recentState.current = value
+  }, [recentState])
+  useLayoutEffect(() => {
+    setState(recentState.current)
+  }, [initialState, recentState, setState])
+  return [state, setState]
+}
